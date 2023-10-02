3f6cb7ce1b81ff85efaaf4e310b0419f22c2e7e1
jstammers <jimmy.stammers@gmail.com>
2023-10-02 12:01:32 +0100

    improved processing script

--- src/earningsai/pipelines/process/nodes.py
+++ src/earningsai/pipelines/process/nodes.py
@@ -12,13 +12,18 @@ LOGGER = logging.getLogger(__name__)
 def combine_records(documents):
     """Combine all records into a single DataFrame"""
     docs = []
+
     for partition_id, partition_load_func in documents.items():
         try:
             data = partition_load_func()
-        except DataSetError:
+        except DataSetError as e:
             LOGGER.error(f"Failed to load {partition_id}")
+            LOGGER.error(e)
             continue
         data["company"] = partition_id.split("_")[2]
-        data["filing_date"] = parse(partition_id.split("_")[3], yearfirst=True)
+        date_section = partition_id.split("_")[3]
+        if "." in date_section:
+            date_section = date_section.split(".")[0]
+        data["filing_date"] = parse(date_section, yearfirst=True)
         docs.append(data)
     return pd.concat(docs, ignore_index=True)
